58c12a69bedb15c53d5ebf3dd377ab2ab0ac09d1
ToruNiina <niina.toru.68u@gmail.com>
2017-05-06 23:40:57 +0900

    add toml::resize func that resize only resizable container

---
+++ tests/test_utility.cpp
@@ -0,0 +1,64 @@
+#define BOOST_TEST_MODULE "test_acceptor"
+#ifdef UNITTEST_FRAMEWORK_LIBRARY_EXIST
+#include <boost/test/unit_test.hpp>
+#else
+#define BOOST_TEST_NO_LIB
+#include <boost/test/included/unit_test.hpp>
+#endif
+#include <toml/utility.hpp>
+#include <vector>
+#include <array>
+
+BOOST_AUTO_TEST_CASE(test_resize)
+{
+    {
+        typedef std::vector<int>  resizable_type;
+        typedef std::array<int,1> non_resizable_type;
+        BOOST_CHECK(toml::detail::has_resize_method<resizable_type>::value);
+        BOOST_CHECK(not toml::detail::has_resize_method<non_resizable_type>::value);
+    }
+
+    {
+        bool thrown = false;
+        std::vector<int> v;
+        try
+        {
+            toml::resize(v, 10);
+        }
+        catch(std::exception& ex)
+        {
+            thrown = true;
+        }
+        BOOST_CHECK(not thrown);
+        BOOST_CHECK_EQUAL(v.size(), 10);
+    }
+
+    {
+        bool thrown = false;
+        std::array<int, 15> a;
+        try
+        {
+            toml::resize(a, 10);
+        }
+        catch(std::exception& ex)
+        {
+            thrown = true;
+        }
+        BOOST_CHECK(not thrown);
+        BOOST_CHECK_EQUAL(a.size(), 15);
+    }
+
+    {
+        bool thrown = false;
+        std::array<int, 15> a;
+        try
+        {
+            toml::resize(a, 20);
+        }
+        catch(std::exception& ex)
+        {
+            thrown = true;
+        }
+        BOOST_CHECK(thrown);
+    }
+}
--- toml/utility.hpp
+++ toml/utility.hpp
@@ -1,5 +1,6 @@
 #ifndef TOML11_UTILITY
 #define TOML11_UTILITY
+#include "traits.hpp"
 #include <utility>
 #include <memory>
 
@@ -10,6 +11,32 @@ template<typename T, typename ... Ts>
 inline std::unique_ptr<T> make_unique(Ts&& ... args)
 {
     return std::unique_ptr<T>(new T(std::forward<Ts>(args)...));
+}
+
+namespace detail
+{
+
+template<typename T>
+inline void resize_impl(T& container, std::size_t N, std::true_type)
+{
+    container.resize(N);
+    return ;
+}
+
+template<typename T>
+inline void resize_impl(T& container, std::size_t N, std::false_type)
+{
+    if(container.size() >= N) return;
+    else throw std::invalid_argument("not resizable type");
+}
+
+}
+
+template<typename T>
+inline void resize(T& container, std::size_t N)
+{
+    if(container.size() == N) return;
+    else return detail::resize_impl(container, N, detail::has_resize_method<T>());
 }
 
 }// toml
