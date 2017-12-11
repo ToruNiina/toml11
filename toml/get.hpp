3083f6549388141a0d02eba79c31a1385a88e67a
ToruNiina <niina.toru.68u@gmail.com>
2017-12-11 12:04:57 +0900

    add get_or

--- toml/get.hpp
+++ toml/get.hpp
@@ -14,7 +14,7 @@ inline T get(const toml::value& v)
     return static_cast<T>(v.cast<vT>());
 }
 
-// array case
+// array-like type
 template<typename T, toml::value_t vT = toml::detail::check_type<T>(),
          typename std::enable_if<(vT == toml::value_t::Unknown) &&
          (!toml::detail::is_map<T>::value) &&
@@ -24,6 +24,7 @@ T get(const toml::value& v)
     if(v.type() != value_t::Array)
         throw type_error("get: value type: " + stringize(v.type()) +
                          std::string(" is not argument type: Array"));
+
     const auto& ar = v.cast<value_t::Array>();
     T tmp;
     try
@@ -39,6 +40,7 @@ T get(const toml::value& v)
     return tmp;
 }
 
+// table-like case
 template<typename T, toml::value_t vT = toml::detail::check_type<T>(),
          typename std::enable_if<(vT == toml::value_t::Unknown) &&
          toml::detail::is_map<T>::value, std::nullptr_t>::type = nullptr>
@@ -51,6 +53,17 @@ T get(const toml::value& v)
     const auto& tb = v.cast<value_t::Table>();
     for(const auto& kv : tb){tmp.insert(kv);}
     return tmp;
+}
+
+//  get_or -----------------------------------------------------------------
+
+template<typename T>
+inline typename std::remove_cv<typename std::remove_reference<T>::type>::type
+get_or(const toml::Table& tab, const toml::key& ky, T&& opt)
+{
+    if(tab.count(ky) == 0) {return std::forward<T>(opt);}
+    return get<typename std::remove_cv<
+        typename std::remove_reference<T>::type>::type>(tab.find(ky)->second);
 }
 
 } // toml
