c313e1382cd64d9bbff43f91602930521fa084f6
ToruNiina <niina.toru.68u@gmail.com>
2019-06-02 22:00:56 +0900

    test: update test for multiple translation units

--- tests/test_multiple_translation_unit_1.cpp
+++ tests/test_multiple_translation_unit_1.cpp
@@ -7,5 +7,5 @@ int main()
     const std::string content("a = 0");
     std::istringstream iss(content);
     const auto data = toml::parse(iss, "test_multiple_translation_unit.toml");
-    return read_a(data);
+    return read_a(toml::get<toml::table>(data));
 }
