86e55c3bf7b3520589bca39aa4b50cd79f2f381d
ToruNiina <niina.toru.68u@gmail.com>
2019-06-18 01:26:16 +0900

    test: check serialization keeps comments

--- tests/test_serialize_file.cpp
+++ tests/test_serialize_file.cpp
@@ -9,7 +9,6 @@
 #include <iostream>
 #include <fstream>
 
-
 BOOST_AUTO_TEST_CASE(test_example)
 {
     const auto data = toml::parse("toml/tests/example.toml");
@@ -31,6 +30,31 @@ BOOST_AUTO_TEST_CASE(test_example)
     BOOST_CHECK(data == serialized);
 }
 
+BOOST_AUTO_TEST_CASE(test_example_with_comment)
+{
+    const auto data = toml::parse<toml::preserve_comments>("toml/tests/example.toml");
+    {
+        std::ofstream ofs("tmp1_com.toml");
+        ofs << std::setw(80) << data;
+    }
+
+    auto serialized = toml::parse<toml::preserve_comments>("tmp1_com.toml");
+    {
+        auto& owner = toml::find<typename decltype(serialized)::table_type>(serialized, "owner");
+        auto& bio   = toml::get<std::string>(owner.at("bio"));
+        const auto CR = std::find(bio.begin(), bio.end(), '\r');
+        if(CR != bio.end())
+        {
+            bio.erase(CR);
+        }
+    }
+    BOOST_CHECK(data == serialized);
+    {
+        std::ofstream ofs("tmp1_com1.toml");
+        ofs << std::setw(80) << serialized;
+    }
+}
+
 BOOST_AUTO_TEST_CASE(test_fruit)
 {
     const auto data = toml::parse("toml/tests/fruit.toml");
@@ -42,6 +66,17 @@ BOOST_AUTO_TEST_CASE(test_fruit)
     BOOST_CHECK(data == serialized);
 }
 
+BOOST_AUTO_TEST_CASE(test_fruit_with_comments)
+{
+    const auto data = toml::parse<toml::preserve_comments>("toml/tests/fruit.toml");
+    {
+        std::ofstream ofs("tmp2_com.toml");
+        ofs << std::setw(80) << data;
+    }
+    const auto serialized = toml::parse<toml::preserve_comments>("tmp2_com.toml");
+    BOOST_CHECK(data == serialized);
+}
+
 BOOST_AUTO_TEST_CASE(test_hard_example)
 {
     const auto data = toml::parse("toml/tests/hard_example.toml");
@@ -50,5 +85,20 @@ BOOST_AUTO_TEST_CASE(test_hard_example)
         ofs << std::setw(80) << data;
     }
     const auto serialized = toml::parse("tmp3.toml");
+    BOOST_CHECK(data == serialized);
+}
+
+BOOST_AUTO_TEST_CASE(test_hard_example_with_comment)
+{
+    const auto data = toml::parse<toml::preserve_comments>("toml/tests/hard_example.toml");
+    {
+        std::ofstream ofs("tmp3_com.toml");
+        ofs << std::setw(80) << data;
+    }
+    const auto serialized = toml::parse<toml::preserve_comments>("tmp3_com.toml");
+    {
+        std::ofstream ofs("tmp3_com1.toml");
+        ofs << std::setw(80) << serialized;
+    }
     BOOST_CHECK(data == serialized);
 }
