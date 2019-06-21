dd9b04ae3b8fb140b0f76b4665cfdce4f310b1b0
ToruNiina <niina.toru.68u@gmail.com>
2019-06-21 15:00:41 +0900

    fix: fix test case name

--- tests/test_traits.cpp
+++ tests/test_traits.cpp
@@ -37,48 +37,48 @@ typedef std::unordered_map<std::string, dummy_type> std_unordered_map_type;
 
 BOOST_AUTO_TEST_CASE(test_has_xxx)
 {
-    BOOST_CHECK(toml::detail::has_iterator<std::list<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::has_iterator<std::forward_list<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::has_iterator<std::deque<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::has_iterator<std::vector<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::has_iterator<std::set<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::has_iterator<std::unordered_set<std::string>>::value);
-    BOOST_CHECK(toml::detail::has_iterator<std_array_type>::value);
-    BOOST_CHECK(toml::detail::has_iterator<std_map_type>::value);
-    BOOST_CHECK(toml::detail::has_iterator<std_unordered_map_type>::value);
-    BOOST_CHECK(toml::detail::has_iterator<dummy_container<dummy_type>>::value);
+    BOOST_TEST(toml::detail::has_iterator<std::list<dummy_type>>::value);
+    BOOST_TEST(toml::detail::has_iterator<std::forward_list<dummy_type>>::value);
+    BOOST_TEST(toml::detail::has_iterator<std::deque<dummy_type>>::value);
+    BOOST_TEST(toml::detail::has_iterator<std::vector<dummy_type>>::value);
+    BOOST_TEST(toml::detail::has_iterator<std::set<dummy_type>>::value);
+    BOOST_TEST(toml::detail::has_iterator<std::unordered_set<std::string>>::value);
+    BOOST_TEST(toml::detail::has_iterator<std_array_type>::value);
+    BOOST_TEST(toml::detail::has_iterator<std_map_type>::value);
+    BOOST_TEST(toml::detail::has_iterator<std_unordered_map_type>::value);
+    BOOST_TEST(toml::detail::has_iterator<dummy_container<dummy_type>>::value);
 
-    BOOST_CHECK(toml::detail::has_value_type<std::list<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::has_value_type<std::forward_list<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::has_value_type<std::deque<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::has_value_type<std::vector<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::has_value_type<std_array_type>::value);
-    BOOST_CHECK(toml::detail::has_value_type<std::set<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::has_value_type<std::unordered_set<std::string>>::value);
-    BOOST_CHECK(toml::detail::has_value_type<std_map_type>::value);
-    BOOST_CHECK(toml::detail::has_value_type<std_unordered_map_type>::value);
-    BOOST_CHECK(toml::detail::has_value_type<dummy_container<dummy_type>>::value);
+    BOOST_TEST(toml::detail::has_value_type<std::list<dummy_type>>::value);
+    BOOST_TEST(toml::detail::has_value_type<std::forward_list<dummy_type>>::value);
+    BOOST_TEST(toml::detail::has_value_type<std::deque<dummy_type>>::value);
+    BOOST_TEST(toml::detail::has_value_type<std::vector<dummy_type>>::value);
+    BOOST_TEST(toml::detail::has_value_type<std_array_type>::value);
+    BOOST_TEST(toml::detail::has_value_type<std::set<dummy_type>>::value);
+    BOOST_TEST(toml::detail::has_value_type<std::unordered_set<std::string>>::value);
+    BOOST_TEST(toml::detail::has_value_type<std_map_type>::value);
+    BOOST_TEST(toml::detail::has_value_type<std_unordered_map_type>::value);
+    BOOST_TEST(toml::detail::has_value_type<dummy_container<dummy_type>>::value);
 
-    BOOST_CHECK(toml::detail::has_key_type<std_map_type>::value);
-    BOOST_CHECK(toml::detail::has_key_type<std_unordered_map_type>::value);
-    BOOST_CHECK(toml::detail::has_mapped_type<std_map_type>::value);
-    BOOST_CHECK(toml::detail::has_mapped_type<std_unordered_map_type>::value);
+    BOOST_TEST(toml::detail::has_key_type<std_map_type>::value);
+    BOOST_TEST(toml::detail::has_key_type<std_unordered_map_type>::value);
+    BOOST_TEST(toml::detail::has_mapped_type<std_map_type>::value);
+    BOOST_TEST(toml::detail::has_mapped_type<std_unordered_map_type>::value);
 }
 
 BOOST_AUTO_TEST_CASE(test_is_xxx)
 {
-    BOOST_CHECK(toml::detail::is_container<std::list<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::is_container<std::forward_list<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::is_container<std::deque<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::is_container<std::vector<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::is_container<std_array_type>::value);
-    BOOST_CHECK(toml::detail::is_container<std::set<dummy_type>>::value);
-    BOOST_CHECK(toml::detail::is_container<std::unordered_set<std::string>>::value);
-    BOOST_CHECK(toml::detail::is_container<dummy_container<dummy_type>>::value);
+    BOOST_TEST(toml::detail::is_container<std::list<dummy_type>>::value);
+    BOOST_TEST(toml::detail::is_container<std::forward_list<dummy_type>>::value);
+    BOOST_TEST(toml::detail::is_container<std::deque<dummy_type>>::value);
+    BOOST_TEST(toml::detail::is_container<std::vector<dummy_type>>::value);
+    BOOST_TEST(toml::detail::is_container<std_array_type>::value);
+    BOOST_TEST(toml::detail::is_container<std::set<dummy_type>>::value);
+    BOOST_TEST(toml::detail::is_container<std::unordered_set<std::string>>::value);
+    BOOST_TEST(toml::detail::is_container<dummy_container<dummy_type>>::value);
 
-    BOOST_CHECK(!toml::detail::is_container<std_map_type>::value);
-    BOOST_CHECK(!toml::detail::is_container<std_unordered_map_type>::value);
+    BOOST_TEST(!toml::detail::is_container<std_map_type>::value);
+    BOOST_TEST(!toml::detail::is_container<std_unordered_map_type>::value);
 
-    BOOST_CHECK(toml::detail::is_map<std_map_type>::value);
-    BOOST_CHECK(toml::detail::is_map<std_unordered_map_type>::value);
+    BOOST_TEST(toml::detail::is_map<std_map_type>::value);
+    BOOST_TEST(toml::detail::is_map<std_unordered_map_type>::value);
 }
